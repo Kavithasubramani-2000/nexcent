82d4eb4df9aa304ff399ed622a666c5ada49b1a8
Kavithasubramani-2000 <varsharithees@gmail.com>
2025-05-10 15:07:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSliderbar()
+{
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar()
+{
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'none'
+}
